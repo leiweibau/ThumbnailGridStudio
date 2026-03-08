11191c3a10822bb95b3a583f8b56a734cfe6bc00
leiweibau <105860611+leiweibau@users.noreply.github.com>
2026-03-08 13:25:31 +0100

    Fix FFmpeg build script array handling under set -u

--- apps/macos/Scripts/build-ffmpeg.sh
+++ apps/macos/Scripts/build-ffmpeg.sh
@@ -23,33 +23,35 @@ build_arch() {
   local ffmpeg_arch="$2"
   local build_dir="$BUILD_ROOT/$arch"
   local install_dir="$INSTALL_ROOT/$arch"
-  local extra_config=()
+  local -a configure_args=()
 
   mkdir -p "$build_dir" "$install_dir"
   rm -rf "$build_dir"/*
 
   if [ "$arch" = "arm64" ]; then
-    extra_config+=(--enable-cross-compile)
+    configure_args+=(--enable-cross-compile)
   fi
+
+  configure_args+=(
+    --prefix="$install_dir"
+    --arch="$ffmpeg_arch"
+    --target-os=darwin
+    --cc=clang
+    --cxx=clang++
+    --extra-cflags="-arch $arch -mmacosx-version-min=$MIN_VERSION -isysroot $SDKROOT"
+    --extra-ldflags="-arch $arch -mmacosx-version-min=$MIN_VERSION -isysroot $SDKROOT"
+    --disable-shared
+    --enable-static
+    --disable-doc
+    --disable-debug
+    --disable-ffplay
+    --disable-network
+    --disable-autodetect
+  )
 
   (
     cd "$build_dir"
-    "$SOURCE_DIR/configure" \
-      --prefix="$install_dir" \
-      --arch="$ffmpeg_arch" \
-      --target-os=darwin \
-      --cc=clang \
-      --cxx=clang++ \
-      --extra-cflags="-arch $arch -mmacosx-version-min=$MIN_VERSION -isysroot $SDKROOT" \
-      --extra-ldflags="-arch $arch -mmacosx-version-min=$MIN_VERSION -isysroot $SDKROOT" \
-      --disable-shared \
-      --enable-static \
-      --disable-doc \
-      --disable-debug \
-      --disable-ffplay \
-      --disable-network \
-      --disable-autodetect \
-      "${extra_config[@]}"
+    "$SOURCE_DIR/configure" "${configure_args[@]}"
     make -j"$(sysctl -n hw.ncpu)"
     make install
   )
